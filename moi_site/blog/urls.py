13e6e2f3a0151cd557520e2f5293b7e6ebc80b7e
Comp <buca-rock@mail.ru>
2022-08-11 16:51:52 +0300

    site_3

--- moi_site/blog/urls.py
+++ moi_site/blog/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog, name="blog"),
+    path("", views.index, name="blog"),
+    path("create", views.create, name="create"),
+    path("create_blog", views.create_blog, name="create_blog"),
    ]
--- moi_site/blog/views.py
+++ moi_site/blog/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
-def blog(request):
+def index(request):
     task = Task.objects.all()
     return render(request,'blog.html', {'title': 'Главная страница', 'tasks':'title'})
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog')
+        else:
+            error = 'Форма была не верной'
+    form = TaskForm()
+    context = {
+        'form':form,
+         'error':error
+    }
+    return render(request,'create.html', context)
+
+def create_blog(request):
+    return render(request,'create_blog.html')
 
 
 
